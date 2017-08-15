72d117f3a04389632e92690178e6d8c5879e2ce3
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-08-15 17:14:06 -0500

    Add working tracker counter

--- nomie_tool.py
+++ nomie_tool.py
@@ -1,6 +1,39 @@
 # Main Nomie processing file
 import nomiepy
+import json
+import os
 
 class NomieComp:
-    def count(self, label, start, end, cached):
-        return { 'count': None, 'message': 'Function not yet built' }
+    NomieInterface = None
+    def __init__(self):
+        config = json.loads(open('config.json').read())
+        self.NomieInterface = nomiepy.Nomie(config['url'], config['username'])
+
+    # Helper methods
+    def __verify_tracker_cache(self):
+        # method is redundant and not of use right now
+        if os.path.isfile('trackernames.json'):
+            return True
+        else:
+            self.NomieInterface.saveTrackers()
+            return True
+
+    # Primary functions
+    def count(self, label, start, end=None, cached=None):
+        events = None
+        if not cached:
+            events = self.NomieInterface.eventList()['rows']
+        else:
+            events = json.loads(open('event_cache.json').read())
+
+	instances = []
+	for i in events:
+	    if i['name'] == label:
+		instances += [i]
+
+        count = 0
+	for i in instances:
+	    if int(i['time']) > int(start):
+		count += 1
+
+        return { 'count': count }
